11cb0ebe777dc5a78b2598c3ae241fac235eefd2
Maruf Niyazov <niyazovmaruf0820@gmail.com>
2023-12-19 13:35:12 +0300

    first commit

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.WriteLine("Enter your FirstName : ");
+string firstname = Console.ReadLine();
+System.Console.WriteLine("Enter your LastName : ");
+string lastname = Console.ReadLine();
+System.Console.WriteLine("Enter your Age : ");
+int age = Convert.ToInt32(Console.ReadLine());
+Person person = new Person();
+person.FirstName = firstname;
+person.LastName = lastname;
+person.Age = age;
+System.Console.WriteLine(person.Information());
+
+class Person
+{
+    public string FirstName;
+    public string LastName;
+    public int Age;
+    public string Information()
+    {
+        return $"FirstName: {FirstName} \nLastName: {LastName} \nAge: {Age}";
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿string text = Console.ReadLine();
+int like = Convert.ToInt32(Console.ReadLine());
+string comment = Console.ReadLine();
+Post post = new Post();
+post.Text = text;
+post.Likes = like;
+post.Coments = comment;
+System.Console.WriteLine(post.SetPost);
+class Post
+{
+    public string Text;
+    public int Likes;
+    public string Coments;
+    public string SetPost(string text)
+    {
+        return $"Text = {Text} \nLikes : {Likes} \nComments : {Coments}";
+    }
+    
+}
